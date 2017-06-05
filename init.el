c23d3bc152f991fcab8f04c643ad62dedc879837
Anler Hernández Peral <anler.hernndez@fidesmo.com>
2017-06-05 09:46:20 +0200

    For the sake of my sanity, just ignore this...

--- init.el
+++ init.el
@@ -60,11 +60,9 @@
       custom-unlispify-names nil
       custom-unlispify-menu-entries nil
 
-      frame-title-format ""
-
       custom-safe-themes t
 
-      frame-title-format '("" "%b @ Emacs " emacs-version)
+      frame-title-format "" ; '("" "%b @ Emacs " emacs-version)
 
       gc-cons-threshold 100000000
       large-file-warning-threshold 100000000
